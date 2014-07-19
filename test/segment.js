8aa3db35549c11b6ac57b9d2a604d5e48afb0a26
myl <myl2821@gmail.com>
2014-07-20 02:42:19 +0800

    add test

--- test/segment.js
+++ test/segment.js
@@ -1,3 +1,6 @@
 var segment = require("../index.js");
 segment.loadDict("./dict/jieba.dict.utf8", "./dict/hmm_model.utf8");
-console.log(segment.cut("南京市长江大桥"));
+segment.cut("非阻塞的南京市长江大桥",  function(res){
+    console.log(res);
+});
+console.log(segment.cutSync("阻塞的南京市长江大桥"));
