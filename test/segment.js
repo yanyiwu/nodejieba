e2d92d862e40d9526803016a1e715d76d6c477af
myl <myl2821@gmail.com>
2014-07-20 14:59:59 +0800

    remove some debug msg

--- test/segment.js
+++ test/segment.js
@@ -1,7 +1,6 @@
 var segment = require("../index.js");
 segment.loadDict("./dict/jieba.dict.utf8", "./dict/hmm_model.utf8");
 segment.cut("非阻塞的南京市长江大桥",  function(tl){
-    	console.log("haha=> "+tl);
 	for(var i = 0; i < tl.length; i++) {
 		console.log(i + " => " + tl[i]);
 	}
