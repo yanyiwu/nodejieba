e31262967349724272b2c4aa0140dd52a89f3669
yuanxindong <yuanxin.me@gmail.com>
2019-09-15 16:43:49 +0800

    fix: space indentation

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,7 +3,7 @@ declare module "nodejieba" {
   export function cut(sentence: string, strict?: boolean): any;
   export function cutHMM(sentence: string): any;
   export function cutAll(sentence: string): any;
-  export function cutForSearch(sentence: string,  strict?: boolean): any;
+  export function cutForSearch(sentence: string, strict?: boolean): any;
   export function tag(sentence: string): any;
   export function extract(sentence: string, threshold: number, allowedPOS?:string): any;
   export function insertWord(sentence: string): any;
