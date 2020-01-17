d3b9de5cfdb6c23ad4dcad86e255000ab69a7bda
Shigma <33423008+Shigma@users.noreply.github.com>
2020-01-17 15:56:02 +0800

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,29 @@
 declare module "nodejieba" {
-  export function load(dict: object): void;
-  export function cut(sentence: string, strict?: boolean): any;
-  export function cutHMM(sentence: string): any;
-  export function cutAll(sentence: string): any;
-  export function cutForSearch(sentence: string, strict?: boolean): any;
-  export function tag(sentence: string): any;
-  export function extract(sentence: string, threshold: number): any;
-  export function insertWord(sentence: string): any;
-  export function cutSmall(sentence: string, small: number): any;
+  export interface LoadOptions {
+    dict?: string;
+    hmmDict?: string;
+    userDict?: string;
+    idfDict?: string;
+    stopWordDict?: string;
+  }
+  
+  export interface TagResult {
+    word: string;
+    tag: string;
+  }
+  
+  export interface ExtractResult {
+    word: string;
+    weight: number;
+  }
+
+  export function load(dict?: LoadOptions): void;
+  export function cut(sentence: string, strict?: boolean): string[];
+  export function cutHMM(sentence: string): string[];
+  export function cutAll(sentence: string): string[];
+  export function cutForSearch(sentence: string, strict?: boolean): string[];
+  export function tag(sentence: string): TagResult[];
+  export function extract(sentence: string, threshold: number): ExtractResult[];
+  export function insertWord(sentence: string): boolean;
+  export function cutSmall(sentence: string, small: number): boolean;
 }
