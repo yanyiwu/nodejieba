348fe83434814be45c4b86ad7ca666393d8cd4ec
Sören Weber <mail@soeren-weber.de>
2020-11-11 14:54:14 +0100

    Make module context-aware to be included in electron Fixes #172

--- lib/index.cpp
+++ lib/index.cpp
@@ -1,24 +1,24 @@
 #include "nodejieba.h"
 
-void init(Local<Object> exports) {
-  Nan::Set(exports,Nan::New<v8::String>("load").ToLocalChecked(),
+NAN_MODULE_INIT(init) {
+  Nan::Set(target,Nan::New<v8::String>("load").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(load)).ToLocalChecked());
-  Nan::Set(exports,Nan::New<v8::String>("cut").ToLocalChecked(),
+  Nan::Set(target,Nan::New<v8::String>("cut").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(cut)).ToLocalChecked());
-  Nan::Set(exports,Nan::New<v8::String>("cutAll").ToLocalChecked(),
+  Nan::Set(target,Nan::New<v8::String>("cutAll").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(cutAll)).ToLocalChecked());
-  Nan::Set(exports,Nan::New<v8::String>("cutHMM").ToLocalChecked(),
+  Nan::Set(target,Nan::New<v8::String>("cutHMM").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(cutHMM)).ToLocalChecked());
-  Nan::Set(exports,Nan::New<v8::String>("cutForSearch").ToLocalChecked(),
+  Nan::Set(target,Nan::New<v8::String>("cutForSearch").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(cutForSearch)).ToLocalChecked());
-  Nan::Set(exports,Nan::New<v8::String>("cutSmall").ToLocalChecked(),
+  Nan::Set(target,Nan::New<v8::String>("cutSmall").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(cutSmall)).ToLocalChecked());
-  Nan::Set(exports,Nan::New<v8::String>("tag").ToLocalChecked(),
+  Nan::Set(target,Nan::New<v8::String>("tag").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(tag)).ToLocalChecked());
-  Nan::Set(exports,Nan::New<v8::String>("extract").ToLocalChecked(),
+  Nan::Set(target,Nan::New<v8::String>("extract").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(extract)).ToLocalChecked());
-  Nan::Set(exports,Nan::New<v8::String>("insertWord").ToLocalChecked(),
+  Nan::Set(target,Nan::New<v8::String>("insertWord").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(insertWord)).ToLocalChecked());
 }
 
-NODE_MODULE(nodejieba, init)
+NAN_MODULE_WORKER_ENABLED(NODE_GYP_MODULE_NAME, init)
--- lib/nodejieba.cpp
+++ lib/nodejieba.cpp
@@ -3,8 +3,6 @@
 #include "cppjieba/Jieba.hpp"
 #include "cppjieba/KeywordExtractor.hpp"
 
-cppjieba::Jieba* global_jieba_handle;
-
 NAN_METHOD(load) {
   if(info.Length() != 5) {
     info.GetReturnValue().Set(Nan::New<v8::Boolean>(false));
@@ -17,12 +15,15 @@ NAN_METHOD(load) {
   Nan::Utf8String idfPath(Nan::To<v8::String>((info[3])).ToLocalChecked());
   Nan::Utf8String stopWordsPath(Nan::To<v8::String>((info[4])).ToLocalChecked());
 
-  delete global_jieba_handle;
-  global_jieba_handle = new cppjieba::Jieba(*dictPath, 
-                                  *modelPath, 
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  delete _jieba_handle;
+  _jieba_handle = new cppjieba::Jieba(*dictPath,
+                                  *modelPath,
                                   *userDictPath,
                                   *idfPath,
                                   *stopWordsPath);
+  Nan::SetIsolateData<cppjieba::Jieba>(isolate, _jieba_handle);
 
   info.GetReturnValue().Set(Nan::New<v8::Boolean>(true));
 }
@@ -41,48 +42,17 @@ NAN_METHOD(insertWord) {
       tag = *(Nan::Utf8String(Nan::To<v8::String>((info[1])).ToLocalChecked()));
   }
  
-  assert(global_jieba_handle);
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  assert(_jieba_handle);
   
-  if(!global_jieba_handle->InsertUserWord(word, tag)) {
+  if(!_jieba_handle->InsertUserWord(word, tag)) {
     info.GetReturnValue().Set(Nan::New<v8::Boolean>(false));
     return;
   }
 
   info.GetReturnValue().Set(Nan::New<v8::Boolean>(true));
 }
-
-//NAN_METHOD(cut) {
-//  if (info.Length() == 0) {
-//    info.GetReturnValue().Set(Nan::New<v8::Boolean>(false));
-//    return;
-//  }
-//  string sentence = *(Nan::Utf8String(Nan::To<v8::String>((info[0])).ToLocalChecked()));
-//  vector<string> words;
-//
-//  assert(global_jieba_handle);
-//  if (info.Length() >= 2) {
-//    string method = *(String::Utf8Value(info[1]->ToString()));
-//    if ("MP" == method) {
-//      global_jieba_handle->Cut(sentence, words, false); 
-//      if (info.Length() == 3) {
-//        global_jieba_handle->CutSmall(sentence, words, info[2]->Int32Value()); 
-//      }
-//    } else if ("HMM" == method) {
-//      global_jieba_handle->CutHMM(sentence, words); 
-//    } else if ("MIX" == method) {
-//      global_jieba_handle->Cut(sentence, words, true); 
-//    } else {
-//      global_jieba_handle->Cut(sentence, words, true); 
-//    }
-//  } else {
-//    global_jieba_handle->Cut(sentence, words, true); 
-//  }
-//
-//  Local<Array> outArray;
-//  WrapVector(words, outArray);
-//
-//  info.GetReturnValue().Set(outArray);
-//}
 
 NAN_METHOD(cut) {
   if (info.Length() == 0) {
@@ -95,7 +65,9 @@ NAN_METHOD(cut) {
   if (info.Length() > 1) {
     useHMM = *Nan::To<v8::Boolean>((info[1])).ToLocalChecked();
   }
-  global_jieba_handle->Cut(sentence, words, useHMM); 
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  _jieba_handle->Cut(sentence, words, useHMM); 
   Local<Array> outArray;
   WrapVector(words, outArray);
   info.GetReturnValue().Set(outArray);
@@ -108,7 +80,9 @@ NAN_METHOD(cutHMM) {
   }
   string sentence = *(Nan::Utf8String(Nan::To<v8::String>((info[0])).ToLocalChecked()));
   vector<string> words;
-  global_jieba_handle->CutHMM(sentence, words); 
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  _jieba_handle->CutHMM(sentence, words); 
   Local<Array> outArray;
   WrapVector(words, outArray);
   info.GetReturnValue().Set(outArray);
@@ -121,7 +95,9 @@ NAN_METHOD(cutAll) {
   }
   string sentence = *(Nan::Utf8String(Nan::To<v8::String>((info[0])).ToLocalChecked()));
   vector<string> words;
-  global_jieba_handle->CutAll(sentence, words); 
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  _jieba_handle->CutAll(sentence, words); 
   Local<Array> outArray;
   WrapVector(words, outArray);
   info.GetReturnValue().Set(outArray);
@@ -138,7 +114,9 @@ NAN_METHOD(cutForSearch) {
   if (info.Length() > 1) {
     useHMM = *Nan::To<v8::Boolean>((info[1])).ToLocalChecked();
   }
-  global_jieba_handle->CutForSearch(sentence, words, useHMM); 
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  _jieba_handle->CutForSearch(sentence, words, useHMM); 
   Local<Array> outArray;
   WrapVector(words, outArray);
   info.GetReturnValue().Set(outArray);
@@ -152,7 +130,9 @@ NAN_METHOD(cutSmall) {
   string sentence = *(Nan::Utf8String(Nan::To<v8::String>((info[0])).ToLocalChecked()));
   vector<string> words;
   size_t word_len_limit =  Nan::To<int32_t>((info[1])).FromJust();
-  global_jieba_handle->CutSmall(sentence, words, word_len_limit); 
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  _jieba_handle->CutSmall(sentence, words, word_len_limit); 
   Local<Array> outArray;
   WrapVector(words, outArray);
   info.GetReturnValue().Set(outArray);
@@ -166,8 +146,10 @@ NAN_METHOD(tag) {
 
   vector<pair<string, string> > words;
   string sentence = *(Nan::Utf8String(Nan::To<v8::String>((info[0])).ToLocalChecked()));
-  assert(global_jieba_handle);
-  global_jieba_handle->Tag(sentence, words); 
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  assert(_jieba_handle);
+  _jieba_handle->Tag(sentence, words); 
 
   Local<Array> outArray;
   WrapPairVector(words, outArray);
@@ -185,8 +167,10 @@ NAN_METHOD(extract) {
   size_t topN = Nan::To<int32_t>((info[1])).FromJust();
   vector<pair<string, double> > words;
 
-  assert(global_jieba_handle);
-  global_jieba_handle->extractor.Extract(sentence, words, topN); 
+  v8::Isolate *isolate = v8::Isolate::GetCurrent();
+  cppjieba::Jieba* _jieba_handle = Nan::GetIsolateData<cppjieba::Jieba>(isolate);
+  assert(_jieba_handle);
+  _jieba_handle->extractor.Extract(sentence, words, topN); 
 
   Local<Array> outArray;
   WrapPairVector(words, outArray);
