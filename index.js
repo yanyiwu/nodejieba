4b5cfd62af6ba853bb2c968cfb6168b9dd7da67e
Sören Weber <mail@soeren-weber.de>
2020-11-18 19:01:17 +0100

    Electron doesn't allow redefinition of exports so we are raising our export object from scratch instead of redefining the nodejieba exports

--- index.js
+++ index.js
@@ -3,47 +3,47 @@ var path = require('path');
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 var nodejieba = require(binding_path);
 
-nodejieba.DEFAULT_DICT = __dirname + "/dict/jieba.dict.utf8";
-nodejieba.DEFAULT_HMM_DICT = __dirname + "/dict/hmm_model.utf8";
-nodejieba.DEFAULT_USER_DICT = __dirname + "/dict/user.dict.utf8";
-nodejieba.DEFAULT_IDF_DICT = __dirname + "/dict/idf.utf8";
-nodejieba.DEFAULT_STOP_WORD_DICT = __dirname + "/dict/stop_words.utf8";
-
 var isDictLoaded = false;
 
-var someFunct = nodejieba.load;
-nodejieba.load = function (dictJson) {
-  if (!dictJson) {
-    dictJson = {}
+var exports ={
+  DEFAULT_DICT: __dirname + "/dict/jieba.dict.utf8",
+  DEFAULT_HMM_DICT: __dirname + "/dict/hmm_model.utf8",
+  DEFAULT_USER_DICT: __dirname + "/dict/user.dict.utf8",
+  DEFAULT_IDF_DICT: __dirname + "/dict/idf.utf8",
+  DEFAULT_STOP_WORD_DICT: __dirname + "/dict/stop_words.utf8",
+
+  load: function (dictJson) {
+    if (!dictJson) {
+      dictJson = {};
+    }
+    dict         = dictJson.dict       || exports.DEFAULT_DICT;
+    hmmDict      = dictJson.hmmDict    || exports.DEFAULT_HMM_DICT;
+    userDict     = dictJson.userDict   || exports.DEFAULT_USER_DICT;
+    idfDict      = dictJson.idfDict    || exports.DEFAULT_IDF_DICT;
+    stopWordDict = dictJson.stopWordDict || exports.DEFAULT_STOP_WORD_DICT;
+
+    isDictLoaded = true;
+    return nodejieba.load(dict, hmmDict, userDict, idfDict, stopWordDict);
   }
-  dict         = dictJson.dict       || nodejieba.DEFAULT_DICT;
-  hmmDict      = dictJson.hmmDict    || nodejieba.DEFAULT_HMM_DICT;
-  userDict     = dictJson.userDict   || nodejieba.DEFAULT_USER_DICT;
-  idfDict      = dictJson.idfDict    || nodejieba.DEFAULT_IDF_DICT;
-  stopWordDict = dictJson.stopWordDict || nodejieba.DEFAULT_STOP_WORD_DICT;
+};
 
-  isDictLoaded = true;
-  return someFunct.call(this, dict, hmmDict, userDict, idfDict, stopWordDict);
-}
-
-function wrapWithDictLoad(obj, functName) {
-  var someFunct = obj[functName];
-  obj[functName] = function () {
+function wrapWithDictLoad(functName) {
+  var someFunct = nodejieba[functName];
+  exports[functName] = function () {
     if (!isDictLoaded) {
-      nodejieba.load();
+      exports.load();
     }
     return someFunct.apply(this, arguments);
   }
 }
 
-wrapWithDictLoad(nodejieba, "cut");
-wrapWithDictLoad(nodejieba, "cutAll");
-wrapWithDictLoad(nodejieba, "cutHMM");
-wrapWithDictLoad(nodejieba, "cutForSearch");
-wrapWithDictLoad(nodejieba, "cutSmall");
-wrapWithDictLoad(nodejieba, "tag");
-wrapWithDictLoad(nodejieba, "extract");
-wrapWithDictLoad(nodejieba, "insertWord");
+wrapWithDictLoad("cut");
+wrapWithDictLoad("cutAll");
+wrapWithDictLoad("cutHMM");
+wrapWithDictLoad("cutForSearch");
+wrapWithDictLoad("cutSmall");
+wrapWithDictLoad("tag");
+wrapWithDictLoad("extract");
+wrapWithDictLoad("insertWord");
 
-module.exports = nodejieba;
-
+module.exports = exports;
